8c69d8c80fb7a7f62c89fd66a34dadaa9d0ad315
seun <oseunodusanya@gmail.com>
2025-05-08 20:44:27 +0100

    sign in sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,40 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { SignedIn, SignedOut, SignOutButton } from "@clerk/nextjs";
+import { SignInButton } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { create } from "domain";
 
 export default function Home() {
+  const files = useQuery(api.files.getFiles)
+  const createFile = useMutation(api.files.createFile)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Button>Hello</Button>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button>Sign in</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <SignOutButton>
+            <Button>Sign out</Button>
+          </SignOutButton>
+        </SignedIn>
+        
+        {files?.map(file => {
+          return <div key={file._id}>{file.name}</div>
+        })}
+        <Button onClick={() =>
+          createFile({
+            name: "hello",
+          })
+        }>Click</Button>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        
       </footer>
     </div>
   );
